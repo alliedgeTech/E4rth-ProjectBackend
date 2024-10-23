a57678ce2630416aaf9726eb0ac1b51211fc4998
Dhyeybhuva2003 <dhyeybhuva2003@gmail.com>
2024-10-23 15:03:31 +0530

    doen

--- routes/ProjectList.routes.js
+++ routes/ProjectList.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const projectController = require("../controllers/ProjectList.controller");
 const { upload, uploadToCloudinary } = require("../config/cloudinary");
+const authenticate = require("../middlewares/authmiddleware");
 const router = express.Router();
 
 const uploadFields = upload.fields([
@@ -10,10 +11,10 @@ const uploadFields = upload.fields([
 ]);
 
 // Routes for creating and updating projects
-router.post("/", uploadFields, projectController.createProject);
-router.put("/:id", uploadFields, projectController.updateProject);
-router.get("/",projectController.getAllProjects);
-router.get("/:id",projectController.getProjectById);
-router.delete("/:id",projectController.deleteProject);
+router.post("/",authenticate, uploadFields, projectController.createProject);
+router.put("/:id",authenticate, uploadFields, projectController.updateProject);
+router.get("/",authenticate,projectController.getAllProjects);
+router.get("/:id",authenticate,projectController.getProjectById);
+router.delete("/:id",authenticate,projectController.deleteProject);
 
 module.exports = router;
--- routes/invest.routes.js
+++ routes/invest.routes.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const investController = require('../controllers/invest.controller');
-
+const express = require("express");
+const investController = require("../controllers/invest.controller");
+const authenticate = require("../middlewares/authmiddleware");
 
 const router = express.Router();
 
 // Create a new investment
-router.post('/investments', investController.createInvestment);
+router.post("/", authenticate, investController.createInvestment);
 
 // Get all investments with filters, pagination, and date range
-router.get('/investments', investController.getAllInvestments);
+router.get("/", authenticate, investController.getAllInvestments);
 
 // Get investment by ID
-router.get('/investments/:id', investController.getInvestmentById);
+router.get("/:id", authenticate, investController.getInvestmentById);
 
 // Delete investment by ID
-router.delete('/investments/:id', investController.deleteInvestment);
+router.delete("/:id", authenticate, investController.deleteInvestment);
 
 module.exports = router;
